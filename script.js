df65ec27ea514de85bf9b284f8300dc091c9db24
NatChapelle <nathchapelle002@gmail.com>
2022-11-19 13:18:26 +0100

    amelioration popup playOnce

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
+let playOnce = true;
+
 window.addEventListener("scroll", () => {
 console.log(scrollY)
-
+ //navbar effect
 if (window.scrollY > 150){
     navbar.style.height = "50px";
 } else {
     navbar.style.height = "90px";
 }
 
-if (window.scrollY > 350){
-    
+// img improvise element
+let scrollValue = ((window.scrollY + window.innerHeight)/document.body.offsetHeight);
+
+// console.log((window.scrollY + window.innerHeight)/document.body.offsetHeight);
+if (scrollValue > 0.45){
     imgImprovise.style.opacity = "1";
-    imgImprovise.style.transform = "translateX(1px)";
-} else {
-    imgImprovise.style.opacity = "0";
-    imgImprovise.style.transform ="translateX(-200px) ";
-}
-if (window.scrollY > 1300) {
+    imgImprovise.style.transform = "none";
+
+} 
+// if (window.scrollY > 350){
+    
+    // imgImprovise.style.opacity = "1";
+    // imgImprovise.style.transform = "translateX(1px)";
+// } else {
+    // imgImprovise.style.opacity = "0";
+    // imgImprovise.style.transform ="translateX(-200px) ";
+// }
+//popup
+// if (window.scrollY > 1300) {
+    // popup.style.opacity = "1";
+    // popup.style.transform = "none";
+    // popup.style.transform = "translateX(-5px)";
+    // popup.style.transitionDelay = ".5s";
+// }else {
+    // popup.style.opacity = "0";
+    
+// }
+if (scrollValue > 0.85 && playOnce) {
     popup.style.opacity = "1";
-    popup.style.transform = "translateX(-5px)";
-    popup.style.transitionDelay = ".5s";
-}else {
-    popup.style.opacity = "0";
+    popup.style.transform = "none";
+    // playOnce = false;
     
 }
 
 })
-window.addEventListener("click", ()=> {
-    console.log("click !");
+
+closeBtn.addEventListener("click", ()=> {
+    
+    popup.style.opacity = "1";
+    popup.style.transform = "translateX(500px)";
+    playOnce = false;
     
 })
 
